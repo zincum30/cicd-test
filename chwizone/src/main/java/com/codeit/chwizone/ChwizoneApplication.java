476d1cc12fbfcaa49e6a772d563357b7bafb59cb
apptie <57691173+apptie@users.noreply.github.com>
2024-06-28 09:49:13 +0900

    chore: #2 애플리케이션 초기 세팅 (#3)
    * chore: 애플리케이션 초기화
    
    * chore: lombok 설정 추가
    
    * chore: application-local 설정 추가
    
    * chore: 로그 설정 추가
    
    * chore: build.gradle 설정 추가
    
    * docs: github pr 템플릿 수정
    
    * fix: 사용하지 않는 로그 설정 삭제

---
+++ chwizone/src/main/java/com/codeit/chwizone/ChwizoneApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.chwizone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwizoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChwizoneApplication.class, args);
+	}
+
+}
---
+++ chwizone/src/test/java/com/codeit/chwizone/ChwizoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.chwizone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChwizoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
