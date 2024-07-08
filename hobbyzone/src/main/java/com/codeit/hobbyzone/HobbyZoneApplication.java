4c6fd6d474d653fa767de05d8cab010f0b3fee7e
practise30 <practise30@gmail.com>
2024-07-08 22:01:30 +0900

    feat: #17 Timezone 변경

--- hobbyzone/src/main/java/com/codeit/hobbyzone/HobbyZoneApplication.java
+++ hobbyzone/src/main/java/com/codeit/hobbyzone/HobbyZoneApplication.java
@@ -1,13 +1,20 @@
 package com.codeit.hobbyzone;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class HobbyZoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HobbyZoneApplication.class, args);
+		SpringApplication.run(HobbyZoneApplication.class, args);}
+
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
